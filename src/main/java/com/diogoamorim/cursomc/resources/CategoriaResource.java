bb2d585527eb27b4e05166670bec179313a9ada8
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-02-27 12:02:56 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/diogoamorim/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diogoamorim/cursomc/resources/CategoriaResource.java
@@ -20,7 +20,7 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = categoriaService.buscar(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj); 
 	}
 
 }
--- src/main/java/com/diogoamorim/cursomc/services/CategoriaService.java
+++ src/main/java/com/diogoamorim/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diogoamorim.cursomc.domain.Categoria;
+import com.diogoamorim.cursomc.exception.ObjectNotFoundException;
 import com.diogoamorim.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> cat = categoriaRepository.findById(id);
-		return cat.orElse(null);
+		return cat.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID" + id + ",Tipo: "
+				+ Categoria.class.getName()));
 	}
 
 }
