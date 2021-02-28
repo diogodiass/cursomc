1fd6786f56b17290f0e8f55327a9d198d5adce9f
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-02-28 11:44:47 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/diogoamorim/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.diogoamorim.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogoamorim.cursomc.domain.Categoria;
+import com.diogoamorim.cursomc.domain.Cliente;
+import com.diogoamorim.cursomc.exception.ObjectNotFoundException;
+import com.diogoamorim.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cli = clienteRepository.findById(id);
+		return cli.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID" + id + ",Tipo: "
+				+ Cliente.class.getName()));
+	}
+
+}
