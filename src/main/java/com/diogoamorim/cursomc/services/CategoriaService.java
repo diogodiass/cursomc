49b8c15d7477a66c5036db55d149f7ea67458b83
Diogo <diogo.dias@inmetrics.com.br>
2021-03-17 15:10:04 -0300

    Paginação com parametros opcionais na requisicao

--- src/main/java/com/diogoamorim/cursomc/services/CategoriaService.java
+++ src/main/java/com/diogoamorim/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.diogoamorim.cursomc.domain.Categoria;
@@ -14,38 +17,42 @@ import com.diogoamorim.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> cat = categoriaRepository.findById(id);
-		return cat.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID" + id + ",Tipo: "
-				+ Categoria.class.getName()));
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID" + id + ",Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return categoriaRepository.save(obj);
 	}
-	
+
 	public Categoria upate(Categoria obj) {
 		find(obj.getId());
 		return categoriaRepository.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		categoriaRepository.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 }
