dd4535c931ff0274b9fd34b5138c94c70008da79
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-03-30 22:00:36 -0300

    Refatoração Service Pedido

--- src/main/java/com/diogoamorim/cursomc/services/PedidoService.java
+++ src/main/java/com/diogoamorim/cursomc/services/PedidoService.java
@@ -46,27 +46,27 @@ public class PedidoService {
 	}
 
 	@Transactional
-	public Pedido insert(Pedido obj) {
-		obj.setId(null);
-		obj.setInstante(new Date());
-		obj.setCliente(clienteService.find(obj.getCliente().getId()));
-		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
-		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
-			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+	public Pedido insert(Pedido pedido) {
+		pedido.setId(null);
+		pedido.setInstante(new Date());
+		pedido.setCliente(clienteService.find(pedido.getCliente().getId()));
+		pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		pedido.getPagamento().setPedido(pedido);
+		if(pedido.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) pedido.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, pedido.getInstante());
 		}
-		obj = pedidoRepository.save(obj);
-		pagamentoRepository.save(obj.getPagamento());
-		for(ItemPedido ip : obj.getItens()) {
+		pedido = pedidoRepository.save(pedido);
+		pagamentoRepository.save(pedido.getPagamento());
+		for(ItemPedido ip : pedido.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
-			ip.setPedido(obj);
-		}
-		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
-		return obj;
+			ip.setPedido(pedido);
+		} 
+		itemPedidoRepository.saveAll(pedido.getItens());
+		System.out.println(pedido);
+		return pedido;
 		
 		
 	}
