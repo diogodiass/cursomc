d35adf407f128596790e81959df1d2af8f933c56
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-05-15 18:02:30 -0300

    Autorizando endpoints para perfil especificos

--- src/main/java/com/diogoamorim/cursomc/services/DBService.java
+++ src/main/java/com/diogoamorim/cursomc/services/DBService.java
@@ -131,6 +131,9 @@ public class DBService {
 		Cliente cli2 = new Cliente(null, "Ana Costa", "testesilva@gmail.com", "314.012.860-62", TipoCliente.PESSOAFISICA,encrypt.encode("4587924"));
 		cli2.addPerfil(Perfil.ADMIN);
 		cli2.getTelefones().addAll(Arrays.asList("27363323", "54484995495"));
+		
+		Cliente cli3 = new Cliente(null, "Dev", "dev@gmail.com", "314.012.860-62", TipoCliente.PESSOAFISICA,encrypt.encode("102030"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
@@ -138,8 +141,9 @@ public class DBService {
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		cli3.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
