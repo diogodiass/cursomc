e5a713c8688d6180a460279c091c4727fcad6621
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-05-15 20:04:26 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/diogoamorim/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/diogoamorim/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,17 @@
 package com.diogoamorim.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.diogoamorim.cursomc.domain.Cliente;
 import com.diogoamorim.cursomc.domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/diogoamorim/cursomc/services/PedidoService.java
+++ src/main/java/com/diogoamorim/cursomc/services/PedidoService.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +15,12 @@ import com.diogoamorim.cursomc.domain.ItemPedido;
 import com.diogoamorim.cursomc.domain.PagamentoComBoleto;
 import com.diogoamorim.cursomc.domain.Pedido;
 import com.diogoamorim.cursomc.domain.enums.EstadoPagamento;
+import com.diogoamorim.cursomc.repositories.ClienteRepository;
 import com.diogoamorim.cursomc.repositories.ItemPedidoRepository;
 import com.diogoamorim.cursomc.repositories.PagamentoRepository;
 import com.diogoamorim.cursomc.repositories.PedidoRepository;
-import com.diogoamorim.cursomc.repositories.ProdutoRepository;
+import com.diogoamorim.cursomc.security.UserSS;
+import com.diogoamorim.cursomc.services.exceptions.AuthorizationException;
 import com.diogoamorim.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,24 +28,26 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
+	
+	
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> ped = pedidoRepository.findById(id);
@@ -55,24 +62,32 @@ public class PedidoService {
 		pedido.setCliente(clienteService.find(pedido.getCliente().getId()));
 		pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		pedido.getPagamento().setPedido(pedido);
-		if(pedido.getPagamento() instanceof PagamentoComBoleto) {
+		if (pedido.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) pedido.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, pedido.getInstante());
 		}
 		pedido = pedidoRepository.save(pedido);
 		pagamentoRepository.save(pedido.getPagamento());
-		for(ItemPedido ip : pedido.getItens()) {
+		for (ItemPedido ip : pedido.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(pedido);
-		} 
+		}
 		itemPedidoRepository.saveAll(pedido.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(pedido);
 		return pedido;
-		
-		
+
 	}
-	
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente  = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
+	}
 
 }
