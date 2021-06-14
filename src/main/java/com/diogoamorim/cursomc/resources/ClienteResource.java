e8cb664cf367fbc56efd5d7bdf0c5a55e451e9d6
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-06-14 20:01:18 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/diogoamorim/cursomc/resources/ClienteResource.java
+++ src/main/java/com/diogoamorim/cursomc/resources/ClienteResource.java
@@ -36,6 +36,12 @@ public class ClienteResource {
 		Cliente obj = clienteService.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(value = "/email", method = RequestMethod.GET)
+	public ResponseEntity<Cliente> findByEmail(@RequestParam(value="value") String email) {
+		Cliente obj = clienteService.findByEmail(email);
+		return ResponseEntity.ok().body(obj);
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
